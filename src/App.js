42f399ea383d377dc55b5ed5cecbdf15c82dcc65
siddharth <siddharthtamboli95@gmail.com>
2023-01-20 23:50:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './List';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const[inputList,setInputList] = useState("");
+  const[list,setList] = useState([]);
+
+  const inputEvent = (event) => {
+      setInputList(event.target.value);
+  };
+  const List_array = () => {
+    setList((oldValue) => {
+      return [...oldValue,inputList];
+    });
+    setInputList("");
+  };
+  const deleteItems = ( id ) => {
+    setList((oldValue) => {
+      return oldValue.filter((arrElem , index) => {
+        return index !== id;
+      })
+    });
+  }
+  return(
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h1>toDo list</h1>
+          <div className='items'>
+          <div className='box'>
+            <input type = "text" placeholder="Add a Items" onChange = {inputEvent} name = "list" value = {inputList}/>
+            <button onClick={List_array}>+</button>
+            </div>
+            <ol>
+                {list.map((itemVal , index) => {
+                  return <List 
+                          id = {index}
+                          key = {index}
+                          text = {itemVal}
+                          onSelect = {deleteItems}
+                          />;
+                })}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
